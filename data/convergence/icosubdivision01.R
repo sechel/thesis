baf128aeda0e4e80c7e4fd7a5756759d63d65e2a
Stefan Sechelmann <sechel@math.tu-berlin.de>
2014-11-25 10:01:23 +0100

    add slope label, new data

--- data/convergence/icosubdivision01.R
+++ data/convergence/icosubdivision01.R
@@ -1,12 +1,14 @@
-data = read.table("icosubdivision.dat")
-pdf('icosubdivision.pdf', width=7.0, height=6.0)
-par(mfrow=c(1,1))
+data = read.table("icosubdivision01.dat")
+pdf('icosubdivision01.pdf', width=7.0, height=6.0)
+par(mfrow=c(1,1), mar=c(5.1, 5.0, 2.1, 2.1))
 x = data$V1
 y = abs(data$V2)
-plot(x, y, log="xy", pch=1, cex=1.0, xlim=c(12,41000), xlab=expression(n), ylab=expression(abs(tau)-hat(abs(tau))))
+plot(x, y, log="xy", pch=1, cex=1.0, xlab=expression(n), ylab=expression(abs(tau)-hat(abs(tau))))
 # create a linear model
 lm = lm(log(y)~log(x))
 print(lm)
 yp = predict(lm, newdata=list(log(x)))
 matlines(x, exp(yp))
+alpha=coefficients(lm)[2]
+text(500,5e-2,labels=bquote(alpha == .(alpha)))
 dev.off()
--- data/convergence/tetrasubdivision01.R
+++ data/convergence/tetrasubdivision01.R
@@ -1,6 +1,6 @@
 data = read.table("tetrasubdivision01.dat")
 pdf('tetrasubdivision01.pdf', width=7.0, height=6.0)
-par(mfrow=c(1,1))
+par(mfrow=c(1,1), mar=c(5.1, 5.0, 2.1, 2.1))
 x = data$V1
 y = abs(data$V2)
 plot(x, y, log="xy", pch=1, cex=1.0, xlab=expression(n), ylab=expression(abs(tau)-hat(abs(tau))))
@@ -9,4 +9,6 @@ lm = lm(log(y)~log(x))
 print(lm)
 yp = predict(lm, newdata=list(log(x)))
 matlines(x, exp(yp))
+alpha=coefficients(lm)[2]
+text(500,2e-2,labels=bquote(alpha == .(alpha)))
 dev.off()
