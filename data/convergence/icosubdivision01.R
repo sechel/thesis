d9509218d05462e74fbdf2833fd64e19c1304320
Stefan Sechelmann <sechel@math.tu-berlin.de>
2014-11-25 15:55:18 +0100

    recalculate icosubdivision data

--- data/convergence/icosubdivision01.R
+++ data/convergence/icosubdivision01.R
@@ -3,7 +3,7 @@ pdf('icosubdivision01.pdf', width=7.0, height=6.0)
 par(mfrow=c(1,1), mar=c(5.1, 5.0, 2.1, 2.1))
 x = data$V1
 y = abs(data$V2)
-plot(x, y, log="xy", pch=1, cex=1.0, xlab=expression(n), ylab=expression(abs(tau)-hat(abs(tau))))
+plot(x, y, log="xy", pch=1, cex=1.0, xlab=expression(n), ylab=expression(abs(tau-hat(tau))))
 # create a linear model
 lm = lm(log(y)~log(x))
 print(lm)
