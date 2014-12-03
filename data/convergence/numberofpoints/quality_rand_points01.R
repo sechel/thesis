b48f76c886d4ee6843218edc6b6f142809f7d647
Stefan Sechelmann <sechel@math.tu-berlin.de>
2014-12-03 13:28:23 +0100

    quality rand plots

--- data/convergence/numberofpoints/quality_rand_points01.R
+++ data/convergence/numberofpoints/quality_rand_points01.R
@@ -3,24 +3,33 @@ pdf('quality_rand_points01.pdf', width=12.0, height=3)
 par(mfrow=c(1,3), mar=c(5.1, 5.0, 2.1, 2.1))
 with(d, 
 	plot(V2, V3, 
-		log='xy', 
+		log='', 
 		xlim=c(20,1500), 
 		ylim=c(1e-4,1e-2), 
 		xlab=expression(n), 
 		ylab=expression(group("|",tau-hat(tau),"|")),
-		main='No quality filtering'
+		main='No quality filtering',
+		pch=20, cex=0.5
 	)
 )
-with(
-	d[d$V14 < 0.03,], 
+df = d[d$V14 < 0.035,]
+with(df, 
 	plot(V2, V3, 
-		log='xy', 
+		log='', 
 		xlim=c(20,1500), 
 		ylim=c(1e-4,1e-2), 
 		xlab=expression(n), 
 		ylab=expression(group("|",tau-hat(tau),"|")),
-		main='Quality filtered MeanMultiRatio < 0.03'
+		main='Quality filtered MeanMultiRatio < 0.03',
+		pch=20, cex=0.5
 	)
 )
+df2 = df[df$V2 > 10,]
+df2 = df2[order(df2$V2),]
+lm <- lm(log(df2$V3) ~ log(df2$V2))
+lines(df2$V2, exp(predict(lm)), type='l', col="red", lwd=2.0)
+alpha=coefficients(lm)[2]
+text(1000, 5e-3, labels=bquote(alpha == .(alpha)))
+
 with(d[d$V14 < 0.2,], hist(V14, breaks=80, main='', xlab='MeanMultiRatio'))
 dev.off()
--- data/convergence/numberofpoints/quality_rand_points02.R
+++ data/convergence/numberofpoints/quality_rand_points02.R
@@ -3,24 +3,33 @@ pdf('quality_rand_points02.pdf', width=12.0, height=3)
 par(mfrow=c(1,3), mar=c(5.1, 5.0, 2.1, 2.1))
 with(d, 
 	plot(V2, V3, 
-		log='xy', 
+		log='', 
 		xlim=c(100,3000), 
-		ylim=c(1e-4,1e-2), 
+		ylim=c(1e-8,7e-3), 
 		xlab=expression(n), 
 		ylab=expression(group("|",tau-hat(tau),"|")),
-		main='No quality filtering'
+		main='No quality filtering',
+		pch=20, cex=0.5
 	)
 )
-with(
-	d[d$V14 < 0.03,], 
+df = d[d$V14 < 0.03,]
+with(df,
 	plot(V2, V3, 
-		log='xy', 
+		log='', 
 		xlim=c(100,3000), 
-		ylim=c(1e-4,1e-2), 
+		ylim=c(1e-8,7e-3),
 		xlab=expression(n), 
 		ylab=expression(group("|",tau-hat(tau),"|")),
-		main='Quality filtered MeanMultiRatio < 0.03'
+		main='Quality filtered MeanMultiRatio < 0.03',
+		pch=20, cex=0.5
 	)
 )
+
+df = df[order(df$V2),]
+lm <- lm(log(df$V3) ~ log(df$V2))
+lines(df$V2, exp(predict(lm)), type='l', col="red", lwd=2.0)
+alpha=coefficients(lm)[2]
+text(1000, 5e-3, labels=bquote(alpha == .(alpha)))
+
 with(d[d$V14 < 0.2,], hist(V14, breaks=80, main='', xlim=c(0, 0.2), xlab='MeanMultiRatio'))
 dev.off()
